28713cf723b2bbbb958f3abbf8cbe8af6d90bd79
Karol <karolp2003@gmail.com>
2024-04-22 12:38:58 +0200

    refactor quizStore

---
+++ src/types/index.ts
@@ -0,0 +1,28 @@
+export interface State {
+  currentQuestionNumber: number;
+  currentScore: number;
+  numberOfQuestions: number;
+  numberOfQuestionsAnswered: number;
+  questions: Question[];
+}
+
+export interface Question {
+  questionNumber: number;
+  questionLabel: string;
+  questionAnsweredIndex: null | 1 | 2 | 3 | 4;
+  questionCorrectIndex: 1 | 2 | 3 | 4;
+  questionTimer: null | string;
+  answers: Answer[];
+}
+
+export interface Answer {
+  answerNumber: 1 | 2 | 3 | 4;
+  answerLabel: string;
+}
+
+export interface Actions {
+  answer: (number: 1 | 2 | 3 | 4) => void;
+  nextQuestion: () => void;
+  previousQuestion: () => void;
+  setQuestionTimer: (time: string) => void;
+}
