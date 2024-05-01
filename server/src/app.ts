362844c3a7ce7dc0899b6abfcaafe412460e8577
Karol <karolp2003@gmail.com>
2024-05-01 14:47:56 +0200

    add errorHandler middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ import "./utils/db";
 import log from "./utils/logger";
 import { PORT } from "./config/index";
 import routes from "./routes";
+import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 
@@ -12,8 +13,10 @@ app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use(express.json());
 
+routes(app);
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   log.info(`Example app listening on port ${PORT}!`);
-
-  routes(app);
 });
