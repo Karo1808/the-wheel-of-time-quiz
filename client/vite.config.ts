510b2323349b0f29f74c67ca389b7e289ec35887
Karol <karolp2003@gmail.com>
2024-05-29 13:22:37 +0200

    remove milion

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
-import MillionLint from "@million/lint";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [MillionLint.vite()],
+  plugins: [react()],
 });
