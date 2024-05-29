b57d479ba5a13ba1407e78bd85d54b2c51b62b35
Karol <karolp2003@gmail.com>
2024-05-29 15:03:44 +0200

    test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import MillionLint from "@million/lint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    MillionLint.vite(),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
