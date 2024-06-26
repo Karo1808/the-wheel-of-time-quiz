c77c9212ce3f250240c545195f7d3391e8697a60
Karol <karolp2003@gmail.com>
2024-06-26 13:03:15 +0200

    improve QuizzesPageSkeleton

--- client/src/pages/loading/QuizzesPageSkeleton.tsx
+++ client/src/pages/loading/QuizzesPageSkeleton.tsx
@@ -10,7 +10,7 @@ const QuizzesPageSkeleton = () => {
   const windowSize = useWindowSize();
   if (!windowSize?.width) return null;
   return (
-    <>
+    <div className={styles.page}>
       <header className={styles.header}>
         <div className={styles.top}>
           <div className={styles.title}>
@@ -37,21 +37,20 @@ const QuizzesPageSkeleton = () => {
               </span>
             )}
           </div>
-          <span className={styles.input_container}>
-            <Skeleton
-              count={1}
-              containerClassName={styles.input}
-              height={35}
-              baseColor={BASE_COLOR}
-              highlightColor={HIGHLIGHT_COLOR}
-              borderRadius={10}
-            />
-          </span>
+          <Skeleton
+            count={1}
+            width={"100%"}
+            height={35}
+            containerClassName={styles.input}
+            baseColor={BASE_COLOR}
+            highlightColor={HIGHLIGHT_COLOR}
+            borderRadius={10}
+          />
         </div>
         <div className={styles.options}>
           <Skeleton
             count={1}
-            height={windowSize.width > 1200 ? 35 : 60}
+            height={windowSize.width > 1200 ? 35 : 45}
             containerClassName={styles.option}
             baseColor={BASE_COLOR}
             highlightColor={HIGHLIGHT_COLOR}
@@ -59,7 +58,7 @@ const QuizzesPageSkeleton = () => {
           />
           <Skeleton
             count={1}
-            height={windowSize.width > 1200 ? 35 : 60}
+            height={windowSize.width > 1200 ? 35 : 45}
             containerClassName={styles.option}
             baseColor={BASE_COLOR}
             highlightColor={HIGHLIGHT_COLOR}
@@ -71,7 +70,7 @@ const QuizzesPageSkeleton = () => {
         {windowSize.width > 1200 && (
           <Skeleton
             count={1}
-            width={200}
+            width={250}
             height={"100%"}
             containerClassName={styles.book_list}
             baseColor={BASE_COLOR}
@@ -98,7 +97,64 @@ const QuizzesPageSkeleton = () => {
           />
         </div>
       </main>
-    </>
+      <footer className={styles.footer}>
+        <Skeleton
+          count={1}
+          height={35}
+          width={35}
+          containerClassName={styles.footer}
+          baseColor={BASE_COLOR}
+          highlightColor={HIGHLIGHT_COLOR}
+          borderRadius={10}
+        />
+        <Skeleton
+          count={1}
+          height={35}
+          width={35}
+          containerClassName={styles.footer}
+          baseColor={BASE_COLOR}
+          highlightColor={HIGHLIGHT_COLOR}
+          borderRadius={10}
+        />
+        <Skeleton
+          count={1}
+          height={35}
+          width={35}
+          containerClassName={styles.footer}
+          baseColor={BASE_COLOR}
+          highlightColor={HIGHLIGHT_COLOR}
+          borderRadius={10}
+        />
+        <Skeleton
+          count={1}
+          height={35}
+          width={35}
+          containerClassName={styles.footer}
+          baseColor={BASE_COLOR}
+          highlightColor={HIGHLIGHT_COLOR}
+          borderRadius={10}
+        />
+        <Skeleton
+          count={1}
+          height={35}
+          width={35}
+          containerClassName={styles.footer}
+          baseColor={BASE_COLOR}
+          highlightColor={HIGHLIGHT_COLOR}
+          borderRadius={10}
+        />
+        <span className={styles.dots}>...</span>
+        <Skeleton
+          count={1}
+          height={35}
+          width={35}
+          containerClassName={styles.footer}
+          baseColor={BASE_COLOR}
+          highlightColor={HIGHLIGHT_COLOR}
+          borderRadius={10}
+        />
+      </footer>
+    </div>
   );
 };
 
